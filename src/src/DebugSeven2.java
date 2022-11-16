619b3f6098d092d0405f9ce23c46e5de9b566f75
Nadhi Woliye <nwoliye@gmail.com>
2022-11-15 18:57:30 -0600

    problem 2 on chapter 7

---
+++ src/src/DebugSeven2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class DebugSeven2 {
+    public static void main(String[] args)
+    {
+        String str;
+        int x;
+        int length;
+        int start = 0;
+        int num;
+        int lastSpace = -1;
+        int sum = 0;
+        String partStr;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a series of integers separated by spaces >> ");
+        str = in.nextLine();
+        length = str.length();
+        for(x = start; x <= length; x++)
+        {
+            if(str.charAt(x) == ' ')
+            {
+                partStr = str.substring(x, lastSpace + 1);
+                num = Integer.parseInt(partStr);
+                System.out.println("                " + num);
+                sum = num;
+                lastSpace == x;
+            }
+        }
+        partStr = str.substring(lastSpace + 1, length);
+        num = Integer.parseInt(partStr);
+        System.out.println("                " + num);
+        sum = num;
+        System.out.println("         -------------------" +
+                "\nThe sum of the integers is " + sum);
+    }
+}
