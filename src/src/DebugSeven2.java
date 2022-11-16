fb06610f74a044754b05c31c7dc5400f283ffaf0
Nadhi Woliye <nwoliye@gmail.com>
2022-11-15 21:05:31 -0600

    solve the problem of the chapter

--- src/src/DebugSeven2.java
+++ src/src/DebugSeven2.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-
+// Program prompts user to enter a series of integers
+// separated by spaces
+// Program converts them to numbers and sums them
 public class DebugSeven2 {
     public static void main(String[] args)
     {
@@ -12,24 +14,25 @@ public class DebugSeven2 {
         int sum = 0;
         String partStr;
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter a series of integers separated by spaces >> ");
+        System.out.println("Enter a series of integers separated by spaces >> ");
         str = in.nextLine();
         length = str.length();
-        for(x = start; x <= length; x++)
+        for(x = 0; x < length; ++x)
         {
             if(str.charAt(x) == ' ')
             {
-                partStr = str.substring(x, lastSpace + 1);
+                partStr = str.substring(lastSpace +1,x);
+
                 num = Integer.parseInt(partStr);
                 System.out.println("                " + num);
-                sum = num;
-                lastSpace == x;
+                sum += num;
+                lastSpace = x;
             }
         }
         partStr = str.substring(lastSpace + 1, length);
         num = Integer.parseInt(partStr);
         System.out.println("                " + num);
-        sum = num;
+        sum += num;
         System.out.println("         -------------------" +
                 "\nThe sum of the integers is " + sum);
     }
